dc02a5c5ffea6dbcc35ad023e16d0c8715525f4f
susmonteiro <susmonteiro@PC-Su.localdomain>
2020-12-06 17:43:16 +0000

    change ideas to readme

---
+++ proj-2/README.md
@@ -0,0 +1,12 @@
+## Ideias:
+#### Arvores mais pequenas 
+- repetir o algoritmo com uma ordem diferente dos atributos
+- podar a arvore -> necessario?
+	 supostamente basta ter uma arvore mais pequena do que aquela do algoritmo base (ver anuncio)
+	 ou seja basta fazer as permutacoes dos atributos e ir correndo ate encontrar uma mais pequena?
+	 mas se houver muitos atributos... como no teste 22, torna resolver este teste muito pesado
+
+#### Ruido
+- dividir os exemplos em conjunto de treino e de testes
+- conjunto de testes tem tamanho 1/k, sendo k o numero de iteracoes e de arvores que se vao gerar 
+- ver qual e a hipotese que tem menor erro no conjunto de teste -> e' aquele que queremos minimizar
--- proj-2/solutions/al089.py
+++ proj-2/solutions/al089.py
@@ -181,20 +181,6 @@ def createdecisiontree(D, Y, noise = False):
 		return DTL(Dlist, Ylist, attr, [])
 
 
-	# arvores mais pequenas 
-	#	- repetir o algoritmo com uma ordem diferente dos atributos
-	#	- podar a arvore -> necessario?
-	# supostamente basta ter uma arvore mais pequena do que aquela do algoritmo base (ver anuncio)
-	# ou seja basta fazer as permutacoes dos atributos e ir correndo ate encontrar uma mais pequena?
-	# mas se houver muitos atributos... como no teste 22, torna resolver este teste muito pesado
-
-	# ruido
-	#	- dividir os exemplos em conjunto de treino e de testes
-	#	- conjunto de testes tem tamanho 1/k, sendo k o numero de iteracoes
-	#	e de arvores que se vao gerar 
-	#	- ver qual e a hipotese que tem menor erro no conjunto de teste -> e' aquele que queremos minimizar
-
-
 
 if __name__ == '__main__':
 	D20 = np.array([[0, 0, 0],
